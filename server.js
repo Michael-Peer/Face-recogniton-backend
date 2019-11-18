8a3072a3f9855fcc00fe90df242cfe80795d10dc
Michael Peer <44434337+Michael-Peer@users.noreply.github.com>
2019-11-18 15:21:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const db = knex({
     connection: {
         host: '127.0.0.1',
         user: 'postgres',
-        password: '122112',
+        password: 'PASSWORD HERE',
         database: 'face-recognition'
     }
 });
